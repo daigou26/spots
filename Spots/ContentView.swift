9b31ec5179e269d449ee9bb8fb8e8d2944eff955
Daigo Uenuma <daigo@MacBook-Air.local>
2021-11-08 05:39:35 +0900

    initial commit

---
+++ Spots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spots
+//
+//  Created by Daigo Uenuma on 2021/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spots/SpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotsApp.swift
+//  Spots
+//
+//  Created by Daigo Uenuma on 2021/11/08.
+//
+
+import SwiftUI
+
+@main
+struct SpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
